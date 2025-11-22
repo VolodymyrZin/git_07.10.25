436dd062185947739e6d6b0d4f84d1fcf7a89e1f
VolodymyrZin <vl.zinych@gmail.com>
2025-11-22 12:07:31 +0200

    Add files via upload
    added dz_211125

---
+++ code.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+import logging
+
+
+def analyze_heartbeat_log(input_file: str, output_file: str):
+    logging.basicConfig(filename=output_file, level=logging.DEBUG,
+                        format='%(asctime)s - %(levelname)s - %(message)s')
+
+    with open(input_file) as f:
+        filtered_log = []
+        for line in f:
+            if "Key TSTFEED0300|7E3E|0400" in line:
+                filtered_log.append(line)
+
+    timestamps = []
+    for log_line in filtered_log:
+        if 'Timestamp ' in log_line:
+            pos = log_line.find("Timestamp ")
+            timestamp_str = log_line[pos + 10:pos + 18]             
+            dt_obj = datetime.strptime(timestamp_str, "%H:%M:%S")
+            timestamps.append(dt_obj)
+
+    for i in range(1, len(timestamps)):
+        diff = abs((timestamps[i] - timestamps[i - 1]).total_seconds())
+
+        if 31 < diff < 33:
+            logging.warning(f"Heartbeat WARNING at {timestamps[i].time()}: {diff} sec")
+        elif diff >= 33:
+            logging.error(f"Heartbeat ERROR at {timestamps[i].time()}: {diff} sec")
+
+
+analyze_heartbeat_log("hblog.txt", "hb_test.log")
